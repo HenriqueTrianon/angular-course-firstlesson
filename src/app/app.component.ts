c69881de81f2cd9f7fcbfdefd9d64c7f0035f8db
Henrique <henrique.trianon@vibetecnologia.com>
2020-07-26 19:37:04 -0300

    Usage of app Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'angular-course';
+  data = {
+    courseTitle: "Angular Core Deep Dive",
+  };
+  onLogoClicked() {
+    alert("Hello World");
+  }
+  onKeyUp(newTitle: string) {
+    this.data.courseTitle = newTitle;
+  }
 }
